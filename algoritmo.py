e84e3eb6a9bcdd8cbd3075fddfa675ca198ec5bb
gara84k <kikeortiz925@gmail.com>
2022-12-09 09:01:05 -0500

    Se agraga el proyecto

---
+++ algoritmo.py
@@ -0,0 +1,6 @@
+ #Crear un bucle que cuente todos los números pares hasta el 100 
+x=1
+while x <= 100:  
+    if x % 2 == 0 :
+        print("El numero",x,"es par")
+    x = x + 1 
---
+++ main.py
@@ -0,0 +1,4 @@
+ #Haz una tabla de multiplicar utilizando el ciclo for 
+for f in range (1,11):
+    multiplicacion = 7* f #hacemos la multiplicacion 
+    print (f'7x{f}= {multiplicacion}') #mostramos el resto de resultado 
